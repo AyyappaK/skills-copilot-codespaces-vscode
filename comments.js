dd2b4d8b31aeeb226abca5320d26bbc8a2005603
Ayyappa Kuncham <kvsayyappa@gmail.com>
2023-08-09 15:16:04 +0000

    commit message

--- comments.js
+++ comments.js
@@ -1,22 +1,3 @@
 // create web server
 // execute node comments.js
 // localhost:3000
-
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-var comments = [];
-http.createServer(function(req, res) {
-  var urlObj = url.parse(req.url, true);
-  var pathname = urlObj.pathname;
-  if (pathname === '/') {
-    fs.readFile('./views/index.html', function(err, data) {
-      if (err) {
-        return res.end('404 Not Found');
-      }
-      var htmlStr = template.render(data.toString(), {
-        comments: comments
-      });
-      res.end(htmlStr);
-    });
